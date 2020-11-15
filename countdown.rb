a2e0d47130a388182ede84cebba77507ad43cd6f
Connor Holland <supersoccergoalie123@gmail.com>
2020-11-15 15:37:23 -0700

    commit tree 8b5e7c7486cd8984fae8f47a2f075649f90de08c

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
 
-def countdown
+def countdown(number)
+    while number > 0
+        puts("#{number} SECOND(S)!")
+        number -= 1
+        # sleep(5)
+    end
+    return "HAPPY NEW YEAR!" if number == 0
 end
+
+countdown(10)
+
+def countdown_with_sleep(number)
+    while number > 0
+        puts("#{number} SECOND(S)!")
+        number -= 1
+        sleep(5)
+    end
+    return "HAPPY NEW YEAR!" if number == 0
+end
+
+countdown_with_sleep(4)
